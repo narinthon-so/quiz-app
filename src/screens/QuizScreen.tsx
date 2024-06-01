ba85e3c3567aff710f54ede08e0b5e90285e0e05
narinthonradiant1 <155409348+narinthonradiant1@users.noreply.github.com>
2024-06-01 11:35:56 +0700

    [fix] handleAnswer when clinked in correct answer multiple time to increase only one score

--- src/screens/QuizScreen.tsx
+++ src/screens/QuizScreen.tsx
@@ -1,3 +1,4 @@
+// src/screens/QuizScreen.tsx
 import React, {useState, useEffect} from 'react';
 import {ScrollView, StyleSheet, Button, Text, View} from 'react-native';
 import QuestionCard from '../components/QuestionCard';
@@ -6,9 +7,15 @@ import {randomizeQuestions} from '../utils/randomize';
 import questions from '../data/questions';
 import {QuizScreenProps} from '../types/navigation';
 
+interface Answer {
+  questionId: string;
+  answerId: string;
+}
+
 const QuizScreen: React.FC<QuizScreenProps> = ({navigation}) => {
   const [quizQuestions, setQuizQuestions] = useState<Question[]>([]);
   const [score, setScore] = useState(0);
+  const [answers, setAnswers] = useState<Answer[]>([]);
 
   useEffect(() => {
     const randomizedQuestions = randomizeQuestions(questions);
@@ -17,8 +24,36 @@ const QuizScreen: React.FC<QuizScreenProps> = ({navigation}) => {
 
   const handleAnswer = (questionId: string, answerId: string) => {
     const question = quizQuestions.find(q => q.id === questionId);
-    if (question && question.correctAnswerId === answerId) {
-      setScore(prevScore => prevScore + 1);
+    const previousAnswer = answers.find(a => a.questionId === questionId);
+
+    if (!question) {
+      return;
+    }
+
+    if (previousAnswer) {
+      if (
+        previousAnswer.answerId === question.correctAnswerId &&
+        answerId !== question.correctAnswerId
+      ) {
+        // Previously correct, now incorrect
+        setScore(prevScore => prevScore - 1);
+      } else if (
+        previousAnswer.answerId !== question.correctAnswerId &&
+        answerId === question.correctAnswerId
+      ) {
+        // Previously incorrect, now correct
+        setScore(prevScore => prevScore + 1);
+      }
+      setAnswers(prevAnswers =>
+        prevAnswers.map(a =>
+          a.questionId === questionId ? {questionId, answerId} : a,
+        ),
+      );
+    } else {
+      if (answerId === question.correctAnswerId) {
+        setScore(prevScore => prevScore + 1);
+      }
+      setAnswers(prevAnswers => [...prevAnswers, {questionId, answerId}]);
     }
   };
 
